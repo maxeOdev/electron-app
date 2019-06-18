afb7254e518af081aa2e5af8e8d6e74e77756fbf
Simon Aliotti <sim.aliotti@gmail.com>
2019-06-18 09:29:47 +0200

    Jdoc

--- renderer.js
+++ renderer.js
@@ -1,15 +1,14 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 const { ipcRenderer } = require('electron');
 const btn = document.getElementById('homedir-list');
 
-//call
+//ipc call
+
+
 btn.addEventListener('click', function () {
     ipcRenderer.send('getList');
 })
 
+//ipc replay
 ipcRenderer.on('getList-reply', (event, arg) => {
     let list ='<ul class="list-group">';
     arg.forEach(element => {
